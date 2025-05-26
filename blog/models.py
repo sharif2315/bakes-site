3f9ae19e93b156828f8e6742155959c01d9151c4
Sharif Ahmed <sharif2315@gmail.com>
2025-05-26 10:42:34 +0100

    recipe models wip

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField, StreamField
+from wagtail.admin.panels import FieldPanel
 
-# Create your models here.
+
+class RecipeIndex(Page):
+    max_count = 1
+    template = "recipes/recipes_index.html"
+    # subpage_types = ['blog.RecipePage']
+
+    subtitle = models.CharField(max_length=100, blank=True)
+    description = RichTextField(
+        blank=True,
+        features=['h2', 'h3', 'h4', 'bold', 'italic', 'ol', 'ul', 'hr']
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('description'),
+    ]
+
