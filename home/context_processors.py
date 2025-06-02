145e52ac10b2c56e1e04713ef35f764b2ee073c0
Sharif Ahmed <sharif2315@gmail.com>
2025-06-02 19:35:52 +0100

    added breadcrumbs

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,11 +1,14 @@
 # home/context_processors.py
 from blog.models import RecipeIndex
 from products.models import ProductListing
+from home.models import HomePage
 
 def custom_context(request):
     product_listing = ProductListing.objects.live().first()
     recipe_index = RecipeIndex.objects.live().first()
+    home_page = HomePage.objects.live().first()
     return {
         'product_listing': product_listing, 
         'recipe_index': recipe_index,
+        'home_page': home_page,
         }
