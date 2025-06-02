8df8a3999f202a7c099961858abc3fa4bdfa48cf
Sharif Ahmed <sharif2315@gmail.com>
2025-06-02 19:40:19 +0100

    updated context preprocessors

--- home/context_processors.py
+++ home/context_processors.py
@@ -3,12 +3,25 @@ from blog.models import RecipeIndex
 from products.models import ProductListing
 from home.models import HomePage
 
+
 def custom_context(request):
-    product_listing = ProductListing.objects.live().first()
-    recipe_index = RecipeIndex.objects.live().first()
-    home_page = HomePage.objects.live().first()
+    product_listing = ProductListing.objects.live().only('title').first()
+    recipe_index = RecipeIndex.objects.live().only('title').first()
+    home_page = HomePage.objects.live().only('title').first()
+
     return {
-        'product_listing': product_listing, 
-        'recipe_index': recipe_index,
-        'home_page': home_page,
-        }
+        'product_listing': {
+            'title': product_listing.title,
+            'url': product_listing.url,
+        } if product_listing else None,
+        
+        'recipe_index': {
+            'title': recipe_index.title,
+            'url': recipe_index.url,
+        } if recipe_index else None,
+        
+        'home_page': {
+            'title': home_page.title,
+            'url': home_page.url,
+        } if home_page else None,
+    }
