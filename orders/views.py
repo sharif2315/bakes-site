84e4a9c446ce0afa88687089e0bd993016514da5
Sharif Ahmed <sharif2315@gmail.com>
2025-06-25 10:30:28 +0100

    checkout cart wip

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,52 @@
+from django.views.decorators.http import require_POST, require_http_methods
 from django.shortcuts import render
+from utils.products import build_cart_context
+
 
 def checkout(request):
     return render(request, 'orders/checkout/checkout.html', {'custom_page_title': 'Checkout' })
+
+
+@require_POST
+def add_to_cart(request, product_id):
+    cart = request.session.get("cart", {})
+    quantity = int(request.POST.get("quantity", 1))
+    product_id_str = str(product_id)
+    cart[product_id_str] = cart.get(product_id_str, 0) + quantity
+
+    request.session["cart"] = cart
+
+    # Return just the updated cart count in cart template partial
+    return render(request, "orders/cart/cart.html", {"cart_total": sum(cart.values())})
+
+
+@require_http_methods(["DELETE"])
+def remove_item_from_cart_slideover(request, product_id):
+    cart = request.session.get("cart", {})
+    product_id_str = str(product_id)
+
+    if product_id_str in cart:
+        del cart[product_id_str]
+
+    request.session["cart"] = cart
+
+    context = build_cart_context(cart)
+    return render(request, "orders/cart/cart.html", context)
+
+
+@require_http_methods(["DELETE"])
+def remove_item_from_checkout(request, product_id):
+    cart = request.session.get("cart", {})
+    product_id_str = str(product_id)
+
+    if product_id_str in cart:
+        del cart[product_id_str]
+
+    request.session["cart"] = cart
+    context = build_cart_context(cart)
+
+    return render(request, "orders/checkout/_order_summary.html", context)
+
 
 """
 fields for Order:
