6911d1e37c02ae3a1e2075f0cd81aad1aace64a2
Sharif Ahmed <sharif2315@gmail.com>
2025-07-05 18:51:07 +0100

    mobile product filters complete

--- static/js/main.js
+++ static/js/main.js
@@ -2,10 +2,13 @@ import '../css/input.css';
 
 import Alpine from "alpinejs";
 import htmx from "htmx.org";
-
+import collapse from '@alpinejs/collapse'
+ 
 
 window.Alpine = Alpine;
 window.htmx = htmx;
+
+Alpine.plugin(collapse)
 
 Alpine.start();
 
